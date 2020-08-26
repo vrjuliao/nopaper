353aa9b9321d39b61619dc9bf0a4e0e74b9de0dd
maTh51 <matheustigon@gmail.com>
2020-08-26 20:14:19 -0300

    Criando tela de login

--- nopaper-frontend/src/pages/Login/index.js
+++ nopaper-frontend/src/pages/Login/index.js
@@ -1,11 +1,77 @@
-import React from 'react';
+import React,{useState} from 'react';
+import { Card, Form, Input, Button } from 'antd';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import { KeyOutlined, MailOutlined } from '@ant-design/icons'
+
+import './styles.css';
+
+
 
 function Login(props) {
+
+  function handleLogin(e){
+    e.preventDefault();
+    alert("Login realizado com sucesso!");
+  };
+
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+
   return (
-    <div style={{ display: 'flex', flex: 1, justifyContent: 'center', alignContent: 'center', alignItems: 'center' }}>
-      <div style={{ backgroundColor: 'white', justifyContent: 'center', alignContent: 'center', alignItems: 'center', padding: 25 }}>
-        <span>Hellooo from nopaper app ;)</span>
-      </div>
+    <div style={{ height: '100vh', display: 'flex', flexDirection: 'column' , flex: 1, justifyContent: 'center', alignContent: 'center', alignItems: 'center' }}>
+      <Card bordered={false} style={{ padding: '50px', display: 'flex', flexDirection: 'column' , justifyContent: 'center', alignContent: 'center', alignItems: 'center' }} >
+        <img src='https://i.vimeocdn.com/portrait/25736037_300x300' width='80px' />
+        <Form onFinish={handleLogin} style={{marginTop: '15px'}}>
+          <Form.Item
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Email!',
+              },
+            ]}
+          >
+            <Input 
+              size='large'
+              prefix={<MailOutlined className="site-form-item-icon" />} 
+              placeholder="Seu melhor e-mail"
+              type='email'
+            />
+          </Form.Item>
+
+          <Form.Item
+            style={{marginTop: '25px'}}
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!',
+              },
+            ]}
+          >
+            <Input.Password
+              size='large' 
+              prefix={<KeyOutlined className="site-form-item-icon" />} 
+              placeholder="Senha"
+              type="password"
+            />
+          </Form.Item>
+
+          <Form.Item>
+            <Button 
+              style={{width: '100%', marginTop: '25px'}}
+              type="primary" 
+              htmlType="submit" 
+              className="login-form-button"
+            >
+              Log in
+            </Button>
+            <p style={{marginTop: '10px', textAlign: 'center'}}>
+              Não tem uma conta? <a href="">Registre-se!</a>
+            </p>
+          </Form.Item>
+        </Form>
+      </Card>
     </div>
   );
 }
