d427bc32281e0524a7ae8ae11ca0aa60b76fb5a1
Luiz <luiz.felipe.556@outlook.com>
2020-09-19 15:04:33 -0300

    created markdown editor page

---
+++ nopaper-frontend/src/pages/MarkdownEditor/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { } from 'antd';
+
+import TopHeader from '../../components/TopHeader';
+
+function MarkdownEditor(props) {
+  return (
+    <div style={{ height: '100vh' }}>
+      <TopHeader />
+    </div>
+  );
+}
+
+export default MarkdownEditor;
