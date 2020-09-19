aa3d6f633edb8ed453864120703715de61c74a6a
Luiz <luiz.felipe.556@outlook.com>
2020-09-19 01:39:45 -0300

    changed backgrounddd

--- nopaper-frontend/src/router.js
+++ nopaper-frontend/src/router.js
@@ -5,6 +5,7 @@ import { hot } from 'react-hot-loader';
 import Login from '../src/pages/Login';
 import Dashboard from '../src/pages/Dashboard';
 
+
 const routes = [
   { path: '/login', component: Login },
   { path: '/dashboard', component: Dashboard }
@@ -17,7 +18,8 @@ const App = () => {
         body {
           margin: 0px;
           padding: 0px;
-          background-image: linear-gradient(to bottom right, rgba(12, 202, 154,0.8), rgba(13, 202, 120,0.5) );
+          background-image: url(./background_2.jpg);
+          background-size: contain
         }
       `}</style>
       <div>
