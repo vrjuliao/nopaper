63b694422d08cf5823e43a41a2ed8fcd0aafa9df
Luiz <luiz.felipe.556@outlook.com>
2020-09-19 20:52:55 -0300

    get notebooks from api

--- nopaper-frontend/src/pages/Dashboard/index.js
+++ nopaper-frontend/src/pages/Dashboard/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Input, Row, Col, Popover, Form, Button } from 'antd';
+import { Input, Row, Col, Popover, Form, Button, Spin } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
-import { notebooks, colors } from './testData';
+import { notebooks as testNotebooks, colors } from './testData';
 import './styles.css';
 import TopHeader from '../../components/TopHeader';
 import Api from '../../modules/api';
@@ -18,19 +18,25 @@ function Dashboard(props) {
 
   const [popoverVisible, setPopoverVisible] = useState(false);
   const [newNotebookName, setNewNotebookName] = useState('');
-
-  const loadApiData = async () => {
-    try {
-      const notebooks = await Api.getUserNotebooks();
-      console.log(notebooks);
-    } catch (err) {
-      console.log('Erro ao tentar encontrar os notebooks');
-    }
-  }
+  const [loading, setLoading] = useState(false);
+  const [notebooks, setNotebooks] = useState([]);
 
   useEffect(() => {
     loadApiData();
   }, [])
+
+  const loadApiData = async () => {
+    setLoading(true);
+    try {
+      const notebooks = await Api.getUserNotebooks();
+      console.log(notebooks);
+      setNotebooks(notebooks);
+      setLoading(false);
+    } catch (err) {
+      console.log('Erro ao tentar encontrar os notebooks');
+      setLoading(false);
+    }
+  }
 
   return (
     <div style={{ height: '100vh' }}>
@@ -40,30 +46,33 @@ function Dashboard(props) {
       <div style={{ alignItems: 'center', height: '-webkit-calc(100% - 95px)', position: 'relative' }} >
         <div style={{ backgroundColor: '#fff', marginLeft: '17.5%', width: '65%', height: '100%', padding: 60, overflow: 'scroll' }}>
 
-        <Row gutter={[16, 32]}>
-          {
-            notebooks && notebooks.map((notebook, index) => {
-              
-              return (
-                <Col key={index} span={8} style={{ }}>
-                  <div style={{ display: 'inline-flex', cursor: 'pointer' }}>
-                    <div style={{ width: 15, height: 145, backgroundColor: colors[index % (colors.length)], borderTopLeftRadius: 8.5 }}>
-                      <div style={{ marginTop: 137, width: 0, height: 0, borderLeft: '7.5px solid transparent', borderRight: '7.5px solid transparent', borderBottom: '8px solid white' }}></div>
+        {
+          loading ? <Spin size="large" /> :
+          <Row gutter={[16, 32]}>
+            {
+              notebooks && notebooks.map((notebook, index) => {
+                
+                return (
+                  <Col key={index} span={8} style={{ }}>
+                    <div style={{ display: 'inline-flex', cursor: 'pointer' }}>
+                      <div style={{ width: 15, height: 145, backgroundColor: colors[index % (colors.length)], borderTopLeftRadius: 8.5 }}>
+                        <div style={{ marginTop: 137, width: 0, height: 0, borderLeft: '7.5px solid transparent', borderRight: '7.5px solid transparent', borderBottom: '8px solid white' }}></div>
+                      </div>
+                      <div style={{ width: 90, height: 125, backgroundColor: 'rgba(97,110,126,0.75)', padding: 10, borderBottomRightRadius: 8.5, borderTopRightRadius: 8.5 }}>
+                        <div style={{ backgroundColor: 'white', height: 15, marginTop: 7, borderRadius: 2 }}></div>
+                        <div style={{ backgroundColor: 'white', height: 5, marginTop: 10, borderRadius: 1 }}></div>
+                      </div>
+                      <div style={{ paddingLeft: 10, whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', height: 125, width: 120 }}>
+                        <span style={{ fontSize: 18, fontWeight: 'bold', color: 'rgba(0,0,0,0.7)', overflow: 'hidden', display: 'inline-block', WebkitLineClamp: 2, WebkitBoxOrient: 'vertical', textOverflow: 'ellipsis' }}>{notebook.name}</span>
+                        <p style={{ fontSize: 12, color: 'rgba(0,0,0,0.4)', marginTop: 30 }}>Criado por<br/>{notebook.author}<br/>{notebook.createdAt}</p>
+                      </div>
                     </div>
-                    <div style={{ width: 90, height: 125, backgroundColor: 'rgba(97,110,126,0.75)', padding: 10, borderBottomRightRadius: 8.5, borderTopRightRadius: 8.5 }}>
-                      <div style={{ backgroundColor: 'white', height: 15, marginTop: 7, borderRadius: 2 }}></div>
-                      <div style={{ backgroundColor: 'white', height: 5, marginTop: 10, borderRadius: 1 }}></div>
-                    </div>
-                    <div style={{ paddingLeft: 10, whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', height: 125, width: 120 }}>
-                      <span style={{ fontSize: 18, fontWeight: 'bold', color: 'rgba(0,0,0,0.7)', overflow: 'hidden', display: 'inline-block', WebkitLineClamp: 2, WebkitBoxOrient: 'vertical', textOverflow: 'ellipsis' }}>{notebook.name}</span>
-                      <p style={{ fontSize: 12, color: 'rgba(0,0,0,0.4)', marginTop: 30 }}>Criado por<br/>{notebook.author}<br/>{notebook.createdAt}</p>
-                    </div>
-                  </div>
-                </Col>
-              );
-            })
-          }
-        </Row>
+                  </Col>
+                );
+              })
+            }
+          </Row>
+        }
 
         <Popover
           title='Adicionar Caderno'
