649e161555c22bcdf2e397bded0d700d166d40d3
Luiz <luiz.felipe.556@outlook.com>
2020-09-20 15:20:55 -0300

    notes page style

--- nopaper-frontend/src/pages/Notes/index.js
+++ nopaper-frontend/src/pages/Notes/index.js
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { Avatar, Card, Button, Tag } from 'antd';
+import { Avatar, Card, Button, Tag, Divider } from 'antd';
 import { PlusOutlined, ArrowLeftOutlined, CopyOutlined, DeleteOutlined, FormOutlined } from '@ant-design/icons'
 import 'antd/dist/antd.css';
 
 import Api from '../../modules/api';
+import TopHeader from '../../components/TopHeader';
 
 import "./styles.css";
+
+const shadow = {
+  WebkitBoxShadow: '10px 3px 74px 17px rgba(0,0,0,0.08)',
+  MozBoxShadow: '10px 3px 74px 17px rgba(0,0,0,0.08)',
+  boxShadow: '10px 3px 74px 17px rgba(0,0,0,0.08)'
+}
 
 function Notes(props){
 
@@ -38,116 +45,119 @@ function Notes(props){
     note: [
       'alooouasdasdsadasdas',
       'meeeu',
-      'booom'
+      'booom',
+      'alooouasdasdsadasdas'
     ]
   }
   
 
   return(
     <div id='page-notes'>
-      <div className='board' style={{ backgroundColor: '#fff', marginLeft: '17.5%', width: '65%', height: '100%' }}>
-        <div className='menu-container'>
-          <Button 
-            // style={}{ width: '100%', marginTop: '25px', borderRadius: 4, paddingTop: 10, paddingBottom: 30  }
-            icon={<ArrowLeftOutlined />}
-            style={{color: "#5b4fff", fontWeight: 'bold', borderColor: '#5b4fff'}}
-            //htmlType="submit" 
-            //className="login-form-button"
-            //onClick={() => handleLogin()}
-          >
-            <span>Voltar</span>
-          </Button>
+      
+      <TopHeader />
 
-          <p>Nome do Caderno</p>
-
-          <Button 
-            // style={}{ width: '100%', marginTop: '25px', borderRadius: 4, paddingTop: 10, paddingBottom: 30  }
-            style={{color: "white", fontWeight: 'bold', borderColor: 'white', backgroundColor: '#5b4fff'}} 
-            icon={<PlusOutlined />}
-            // onClick={() => {
-            //   createNote();
-            // }}
-            //htmlType="submit" 
-            //className="login-form-button"
-            //onClick={() => handleLogin()}
-          >
-            <span>Nova nota</span>
-          </Button>
-        </div>
+      <div style={{ backgroundColor: '#fff', marginLeft: '17.5%', width: '65%', height: '-webkit-calc(100% - 95px)', overflow: 'scroll', padding: 20, }}>
         
-        <div className="avatar-container">
-          <Avatar style={{ backgroundColor: 'greenyellow', fontSize: 75, marginBottom: '20px'}} size={160}>L</Avatar>
-          <span style={{marginBottom: '10px'}}>
-            <Tag 
-              color="#bfbfbf"
-              style={{color: '#000000', borderRadius: '50px', fontWeight: 'bold'}}
-            >
-              {notebook.name}
-            </Tag>
-          </span>
-          <span style={{marginBottom: '10px'}}>
-            <Tag 
-              color="#bfbfbf"
-              style={{color: '#000000', borderRadius: '50px', fontWeight: 'bold'}}
-            >{notebook.user}</Tag>
-          </span>
-          <span style={{marginBottom: '10px'}}>
-            <Tag
-              color="#bfbfbf"
-              style={{color: '#000000', borderRadius: '50px', fontWeight: 'bold'}}
-            >{notebook.date}</Tag>
-          </span>
-          <p>
-            <Button 
-              style={{borderColor: "lightgrey"}} 
-              icon={<FormOutlined style={{ color: '#000000', fontSize: 30 }} />} 
-            />
-            <Button 
-              style={{borderColor: "lightgrey"}}
-              icon={<CopyOutlined style={{ color: '#7d7d7d', fontSize: 30 }}/>} 
-            />
-            <Button 
-              style={{borderColor: "red"}}
-              icon={<DeleteOutlined style={{ color: '#ff584f', fontSize: 30 }}/>} 
-            />
-          </p>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+
+          <div style={{ alignItems: 'center', backgroundColor: '#2fa8d4', paddingLeft: 20, paddingRight: 40, paddingTop: 8, paddingBottom: 8, borderRadius: 5, cursor: 'pointer', display: 'flex' }}>
+            <ArrowLeftOutlined style={{ fontSize: 15, color: 'white' }} />
+            <span style={{ color: 'white', fontWeight: 'bold', marginLeft: 10 }}>Voltar</span>
+          </div>
+
+          <div style={{ alignItems: 'center', backgroundColor: '#2fa8d4', paddingLeft: 20, paddingRight: 40, paddingTop: 8, paddingBottom: 8, borderRadius: 5, cursor: 'pointer', display: 'flex' }}>
+            <PlusOutlined style={{ fontSize: 15, color: 'white' }} />
+            <span style={{ color: 'white', fontWeight: 'bold', marginLeft: 10 }}>Criar nota</span>
+          </div>
+
         </div>
 
-        
-        <div className="note-container">
-          {notebook.note.map((string, i) => {
-            return(
-              <Card style={{ width: 450, marginTop: 16, marginLeft: 20, }}
-                title={string}
-                headStyle={{fontWeight: 'bold'}}
-                bodyStyle={{fontWeight: 'bold'}}
-                extra={
-                  <div>
-                    <Button
-                      style={{borderColor: "lightgrey"}} 
-                      icon={<FormOutlined style={{ color: '#000000', fontSize: 20}}/>} />
-                    <Button
-                      style={{borderColor: "lightgrey"}}
-                      icon={<CopyOutlined style={{ color: '#7d7d7d', fontSize: 20}}/>} />
-                    <Button 
-                      style={{borderColor: "red"}}
-                      icon={<DeleteOutlined style={{ color: '#ff584f', fontSize: 20 }}/>} />
+        <div style={{ display: 'flex', flex: 3, marginTop: 25 }}>
+
+          <div style={{ textAlign: 'left', flex: 1,  padding: 20, display: 'flex', flexDirection: 'column' }}>
+            
+            <div style={{ alignContent: 'center', justifyContent: 'center', display: 'flex' }}>
+              <Avatar style={{ backgroundColor: 'greenyellow', fontSize: 75, marginBottom: '20px'}} size={130}>L</Avatar>
+            </div>
+            
+            <span style={{ fontSize: 20, fontWeight: 'bold', color: 'rgba(0,0,0,0.8)' }}>{notebook.name}</span>
+            <span style={{ fontSize: 20, color: 'rgba(0,0,0,0.4)' }}>{notebook.user}</span>
+            <span style={{ fontSize: 20, color: 'rgba(0,0,0,0.4)' }}>Criado em {notebook.date}</span>
+
+            <div style={{ alignContent: 'center', justifyContent: 'space-around', display: 'flex', marginTop: 15 }} >
+              <div style={{ padding: 8, border: '2px solid #2fa8d4', borderRadius: 50 }}>
+                <Button 
+                  style={{ border: '0px' }}
+                  icon={<FormOutlined style={{ color: '#2fa8d4', fontSize: 20, marginTop: 4 }} />} 
+                />
+              </div>
+
+              <div style={{ padding: 8, border: '2px solid #2fa8d4', borderRadius: 50 }}>
+                <Button 
+                  style={{ border: '0px' }}
+                  icon={<CopyOutlined style={{ color: '#2fa8d4', fontSize: 20, marginTop: 4 }}/>} 
+                />
+              </div>
+
+              <div style={{ padding: 8, border: '2px solid #ff584f', borderRadius: 50 }}>
+                <Button 
+                  style={{ border: '0px' }}
+                  icon={<DeleteOutlined style={{ color: '#ff584f', fontSize: 20, marginTop: 4 }}/>} 
+                />
+              </div>
+            </div>
+
+          </div>
+
+          <div style={{ flex: 2, paddingLeft: 20 }}>
+            {
+              notebook.note.map((note, index) => {
+                return (
+                  <div style={{ marginTop: index != 0 && 20, borderRadius: 6, padding: 7, paddingBottom: 15, ...shadow }}>
+
+                    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', paddingLeft: 15 }}>
+                      <span style={{ fontSize: 18 }}>{note}</span>
+
+                      <div style={{ alignContent: 'center', justifyContent: 'space-around', display: 'flex' }} >
+                        <div style={{ padding: 0, border: '0px solid #2fa8d4' }}>
+                          <Button 
+                            style={{ border: '0px', backgroundColor: 'transparent' }}
+                            icon={<FormOutlined style={{ color: '#2fa8d4', fontSize: 17, marginTop: 4 }} />} 
+                          />
+                        </div>
+
+                        <div style={{ padding: 0, border: '0px solid #2fa8d4' }}>
+                          <Button 
+                            style={{ border: '0px', backgroundColor: 'transparent' }}
+                            icon={<CopyOutlined style={{ color: '#2fa8d4', fontSize: 17, marginTop: 4 }}/>} 
+                          />
+                        </div>
+
+                        <div style={{ padding: 0, border: '0px solid #ff584f' }}>
+                          <Button 
+                            style={{ border: '0px', backgroundColor: 'transparent' }}
+                            icon={<DeleteOutlined style={{ color: '#ff584f', fontSize: 17, marginTop: 4 }}/>} 
+                          />
+                        </div>
+                      </div>
+
+                    </div>
+
+                    <div style={{ width: '30%' , paddingLeft: 15 }}>
+                      <Divider style={{ padding: 0, margin: 0, marginTop: 5, marginBottom: 5, height: 8 }} />
+                    </div>
+
+                    <div style={{ paddingLeft: 15, marginTop: 7 }}>
+                      <Tag color="volcano">{notebook.date}</Tag>
+                    </div>
+
                   </div>
-                }
-              >
-                <span>
-                  <Tag 
-                    color="#bfbfbf"
-                    style={{color: '#000000', borderRadius: '50px'}}
-                  >
-                    {notebook.date}
-                  </Tag>
-                </span>
-              </Card>
-            )
-          })}
-        </div>
+                );
+              })
+            }
+          </div>
 
+        </div>
       </div>
     </div>
   )
