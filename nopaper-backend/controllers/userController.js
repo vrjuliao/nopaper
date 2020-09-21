a3644eee641c3a86cdb3db459ed5b071f9cf639d
V. Julião <viniciusjuliao@dcc.ufmg.br>
2020-09-20 23:14:10 -0300

    backend(users): finxing route to get all users except the current user

--- nopaper-backend/controllers/userController.js
+++ nopaper-backend/controllers/userController.js
@@ -1,7 +1,9 @@
 var User = require('../models/userModel');
 
 exports.all = function (req, res){
-    User.find({}, ['username', 'email', 'name', '_id'],function (err, success){
+    User.find({
+        _id: {$ne: req.body.userId}
+    }, ['username', 'email', 'name', '_id'],function (err, success){
         if(err)
             return res.status(400).send('Usuários não obtidos');
         return res.send(success);
